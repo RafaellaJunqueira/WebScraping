675e295d92a0f0f78b203d08290edfe141ab658e
RafaellaJunqueira <rafaellafjunqueira@gmail.com>
2022-08-16 23:05:40 -0300

    [exp 1] Coloca o time para medir tempo de execucao

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Web Scraping
 Este repositório destina-se aos scripts de extração de dados, utilizando ferramentas e tecnologias para web scraping, como parte prática do Trabalho de Conclusão de Curso 2 em Engenharia de Software.
-
+<!-- 
 ## Referências - dicas
 [Site para baixar os driver](https://github.com/mozilla/geckodriver/releases)
 
 [Video sobre webdriver](https://www.youtube.com/watch?v=Ot10qzrb13c)
-
+ -->
--- SeleniumSelenium/SeleniumSelenium.py
+++ SeleniumSelenium/SeleniumSelenium.py
@@ -1,4 +1,5 @@
 import time
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -6,6 +7,7 @@ def search_name(**in_args):
     user_input = in_args['name']
     driver = webdriver.Chrome()
     driver.get('https://portal.stf.jus.br/')
+    sleep(2)
     driver.find_element(By.XPATH, '//option[@value="PARTE_OU_ADVOGADO"]').click()
     driver.find_element(By.ID, 'pesquisaPrincipalParteAdvogado').send_keys(user_input)
     driver.find_element(By.ID, 'btnPesquisar').click()
@@ -23,8 +25,11 @@ def search_name(**in_args):
                 temp_dict[headers[index].text] = cell.text
         result_list.append(temp_dict.copy())
 
-    print(result_list)
+    print(f'RESULTADO: {result_list}')
 
 if __name__ == '__main__':
+    start = time.time()
     args = {"name": "Vitor Borges"}
     search_name(**args)
+    end = time.time()
+    print(f'TEMPO DE EXECUÇÃO:{end - start}')
