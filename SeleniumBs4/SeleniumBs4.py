05d3b053a4918dd1c0cf59a669422fbe17c646e3
RafaellaJunqueira <rafaellafjunqueira@gmail.com>
2022-08-16 21:47:07 -0300

    [exp 1] Abrindo navegador e clicando com selenium

--- SeleniumBs4/SeleniumBs4.py
+++ SeleniumBs4/SeleniumBs4.py
@@ -1,7 +1,18 @@
+from asyncio import selector_events
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-browser = webdriver.Firefox(executable_path='./selenium/geckodriver')
-browser.get('http://www.ubuntu.com/')
+driver = webdriver.Chrome()
 
+driver.get('https://portal.stf.jus.br/')
+
+# select_box = '//select[@class="campo-texto tipo-pesquisa-processo"]'
+# driver.find_element(By.XPATH, select_box).click()
+
+driver.find_element(By.XPATH, '//option[@value="PARTE_OU_ADVOGADO"]').click()
+driver.find_element(By.ID, 'pesquisaPrincipalParteAdvogado').send_keys('Vitor Borges')
+driver.find_element(By.ID, 'btnPesquisar').click()
+
+print('teste')
 if __name__ == '__main__':
     print("oi")
